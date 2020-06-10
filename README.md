a136dc735148aa46b59f9074cb963380d7910789
Brian Dixon <brianide@gmail.com>
2020-06-10 14:21:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SeededDrunkWorld
+A TShock plugin that allows "drunk" worlds to be generated with a static seed.
+
+## Details
+After installing the plugin, simple generate a new world and select **Random** when prompted to choose the world evil. You may then provide a seed as normal; the world will generate from this seed with drunk features.
---
+++ SeededDrunkWorld/SeededDrunkWorldPlugin.cs
@@ -0,0 +1,33 @@
+﻿using OTAPI;
+using System;
+using Terraria;
+using Terraria.WorldBuilding;
+using TerrariaApi.Server;
+
+namespace SeededDrunkWorld
+{
+    [ApiVersion(2, 1)]
+    public class SeededDrunkWorldPlugin : TerrariaPlugin
+    {
+        public override string Name => "Seeded Drunk World";
+        public override string Description => "Allows all world seeds to generate drunk";
+        public override string Author => "gigabarn";
+        public override Version Version => new Version(0, 1);
+
+        public SeededDrunkWorldPlugin(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            Terraria.WorldGen.Hooks.OnWorldGenConfigProcess += (ref WorldGenConfiguration config) =>
+            {
+                if (Terraria.WorldGen.WorldGenParam_Evil < 0)
+                {
+                    Console.WriteLine("Random world evil was selected; a seeded drunk world will be generated");
+                    Terraria.WorldGen.drunkWorldGen = true;
+                }
+            };
+        }
+    }
+}
